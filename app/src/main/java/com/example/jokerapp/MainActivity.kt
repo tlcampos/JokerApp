3ffa454acf6e4ba6e3c4f577e54c400f39415b57
Taffarel Campos <taffarel_lc@hotmail.com>
2024-04-02 11:26:01 -0300

    Sincronizando telas e menus com controlador

--- app/src/main/java/com/example/jokerapp/MainActivity.kt
+++ app/src/main/java/com/example/jokerapp/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.jokerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.jokerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = binding.appBarMain.tollbar
+        setSupportActionBar(toolbar)
+
+        val drawerLayout = binding.drawerLayout
+        val navView = binding.navView
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment?
+        val navController = navHostFragment!!.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_joke_day,
+                R.id.nav_about
+            ), drawerLayout
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(this.appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
